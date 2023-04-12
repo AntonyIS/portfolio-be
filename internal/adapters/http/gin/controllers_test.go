3b31f3c50df82538057fbf1ab8075a52c88f7f31
AntonyIS <antonyshikubu@gmail.com>
2023-04-12 23:51:06 +0300

    Adding initial tests for Gin router

--- internal/adapters/http/gin/controllers_test.go
+++ internal/adapters/http/gin/controllers_test.go
@@ -1,2 +1,70 @@
 package gin
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/AntonyIS/portfolio-be/config"
+	"github.com/AntonyIS/portfolio-be/internal/adapters/repository"
+	"github.com/AntonyIS/portfolio-be/internal/core/domain"
+	"github.com/AntonyIS/portfolio-be/internal/core/services"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+func TestApplicationRoutes(t *testing.T) {
+	config := &config.AppConfig{
+		Env:          "Dev",
+		Port:         "3000",
+		UsersTable:   "Authors",
+		ProjectTable: "Projects",
+		Region:       "me-south-1",
+	}
+	repo := repository.NewDynamoDBRepository(config)
+	svc := services.NewPortfolioService(&repo)
+	handler := NewGinHandler(*svc)
+	t.Run("Gin Post user", func(t *testing.T) {
+		r := SetUpRouter()
+		r.POST("/v1/users", handler.PostUser)
+
+		newUser := domain.User{
+			FirstName: "Marco",
+			LastName:  "Injila",
+			Email:     "marco@gmail.com",
+			Title:     "Golang Software Engineer",
+			Password:  "password",
+			Projects:  nil,
+		}
+		jsonValue, _ := json.Marshal(newUser)
+		req, _ := http.NewRequest("POST", "/v1/users", bytes.NewBuffer(jsonValue))
+
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusCreated, w.Code)
+	})
+	t.Run("Gin Read user with email", func(t *testing.T) {
+		r := SetUpRouter()
+		email := "marco@gmail.com"
+		url := fmt.Sprintf("/v1/users/%s", email)
+		r.GET(url, handler.GetUser)
+
+		req, _ := http.NewRequest("GET", url, nil)
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+
+		var user *domain.User
+		json.Unmarshal(w.Body.Bytes(), user)
+		fmt.Println(user)
+		assert.Equal(t, http.StatusOK, w.Code)
+
+	})
+
+}
